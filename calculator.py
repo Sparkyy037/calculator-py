3d6e6b4e3774faf3f3415d83b305cb3ba008a927
Sparkyy037 <144593060+Sparkyy037@users.noreply.github.com>
2023-09-10 15:38:47 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,11 @@
+a = int(input("Enter First no :"))
+b = int(input("Enter Second no"))
+var = str(input("Enter variable(+,-,*,/):"))
+if var == "+" :
+  print(a+b)
+if var == "-" :
+  print(a-b)
+if var == "/" :
+  print(a/b)
+if var == "*" :
+  print(a*b)
